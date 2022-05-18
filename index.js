42200a9c8ba7bbac26875a33e3a91560c542dd4d
AzmiHabib <azmihabib0323@gmail.com>
2022-05-19 01:46:48 +0600

    commit after database

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,18 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 // token verify
-const tokenVerify = (req, res, next) => {
-  const token = req.headers.authorization;
-  jwt.verify(token, process.env.SECRETE_CODE, function (err, decoded) {
-    if (err) {
-      console.log(err);
-      res.send({ message: err }); //have to done
-      return;
-    }
-    req.decoded = decoded;
-    next();
-  });
-};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ee5t8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -31,9 +18,36 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+const collection = client.db("todoCollection").collection("todo");
+
 async function run() {
   try {
-   
+    await client.connect()
+    console.log('database connect');
+    app.get("/todo", async (req, res) => {
+      const result = await collection.find().toArray();
+      res.send(result);
+    });
+    app.delete("/todo/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await collection.deleteOne({ _id: ObjectId(id) });
+      res.send(result);
+    });
+    app.post("/todo", async (req, res) => {
+      const todo = req.body;
+      const result = await collection.insertOne({ todo });
+      res.send(result);
+    });
+    app.put("/todo/:id", async (req, res) => {
+      const id = req.params.id;
+      const done = req.body;
+      const filter = { _id: ObjectId(id) };
+      const update = { $set: { done } };
+      const result = await collection.updateOne(filter, update, {
+        upsert: true,
+      });
+      res.send(result);
+    });
 
     // finally
   } finally {
